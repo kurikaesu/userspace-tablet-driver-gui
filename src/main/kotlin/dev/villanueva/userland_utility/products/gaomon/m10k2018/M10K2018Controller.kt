a7284cd73fed34bf861bb61ec4c5221d83444f58
TheLastBilly <jtmonegro@gmail.com>
2022-01-30 16:12:42 -0500

    added support for Gaomon M10K 2018

---
+++ src/main/kotlin/dev/villanueva/userland_utility/products/gaomon/m10k2018/M10K2018Controller.kt
@@ -0,0 +1,58 @@
+package dev.villanueva.userland_utility.products.gaomon.m10kpro
+
+import dev.villanueva.userland_utility.products.DriverCodeIDs
+import dev.villanueva.userland_utility.products.MappableItem
+import dev.villanueva.userland_utility.products.MappableItemType
+import dev.villanueva.userland_utility.products.ProductController
+import dev.villanueva.userland_utility.products.config.Configuration
+import dev.villanueva.userland_utility.products.config.DeviceConfiguration
+
+class M10K2018Controller : ProductController() {
+    init {
+        mapItems.add(MappableItem(MappableItemType.Button, "K1", DriverCodeIDs.BTN_0.code, 0))
+        mapItems.add(MappableItem(MappableItemType.Button, "K2", DriverCodeIDs.BTN_1.code, 0))
+        mapItems.add(MappableItem(MappableItemType.Button, "K3", DriverCodeIDs.BTN_2.code, 0))
+        mapItems.add(MappableItem(MappableItemType.Button, "K4", DriverCodeIDs.BTN_3.code, 0))
+        mapItems.add(MappableItem(MappableItemType.Button, "K5", DriverCodeIDs.BTN_4.code, 0))
+        mapItems.add(MappableItem(MappableItemType.Dial, "TouchDial -> Left", DriverCodeIDs.REL_WHEEL.code, -1))
+        mapItems.add(MappableItem(MappableItemType.Button, "DialButton", DriverCodeIDs.BTN_SOUTH.code, 0))
+        mapItems.add(MappableItem(MappableItemType.Dial, "TouchDial -> Right", DriverCodeIDs.REL_WHEEL.code, 1))
+        mapItems.add(MappableItem(MappableItemType.Button, "K6", DriverCodeIDs.BTN_5.code, 0))
+        mapItems.add(MappableItem(MappableItemType.Button, "K7", DriverCodeIDs.BTN_6.code, 0))
+        mapItems.add(MappableItem(MappableItemType.Button, "K8", DriverCodeIDs.BTN_7.code, 0))
+        mapItems.add(MappableItem(MappableItemType.Button, "K9", DriverCodeIDs.BTN_8.code, 0))
+        mapItems.add(MappableItem(MappableItemType.Button, "K10", DriverCodeIDs.BTN_9.code, 0))
+    }
+
+    override fun updateExistingDeviceConfig(deviceConfiguration: DeviceConfiguration): Configuration {
+        val existingConfig = Configuration.readConfig()
+        existingConfig.deviceConfigurations[getVendorIdAsString()]!![getProductIdAsString()] = deviceConfiguration
+        return existingConfig
+    }
+
+    companion object {
+        private fun getProductId(): Short {
+            return 281
+        }
+
+        private fun getVendorId(): Short {
+            return 9580
+        }
+
+        fun getProductIdAsString(): String {
+            return getProductId().toString()
+        }
+
+        fun getVendorIdAsString(): String {
+            return getVendorId().toString()
+        }
+
+        fun getVendorProductString(): String {
+            return "${getVendorId()}:${getProductId()}"
+        }
+
+        fun getProductName(): String {
+            return "Gaomon M10K 2018"
+        }
+    }
+}
